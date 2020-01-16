d92867d31c0baf3cebd5b4ea181365562df9d980
JerseyKidG <cz284@njit.edu>
2020-01-15 22:32:30 -0600

    State: Initializing State Through Constructors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//functional based component
-/* const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
-
-
-    return <div>Latitude: </div>;
-}; */
-
-//class based component
 class App extends React.Component {
+    constructor(props) {
+        super(props); //a reference to the parents constructor function
+
+        this.state = { lat: null };
+    }
+
+     // React says we have to define render!!
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
