c70bdffb1d7c3d2c2270462aa79181d58142116c
JerseyKidG <cz284@njit.edu>
2020-01-15 22:51:33 -0600

    Update State Properties

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     constructor(props) {
-        super(props); //a reference to the parents constructor function
+        super(props); //a reference to the parents constructor function, must have this
 
+        //THIS IS THE ONLY TIME we do direct assignment to this.state
         this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we called setstate !!!
+                this.setState({ lat: position.coords.latitude });
+
+                //we did not, never do this 
+                //this.state.lat = position.coords.latitude
+            },
+            err => console.log(err)
+        );
     }
 
      // React says we have to define render!!
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
-
-        return <div>Latitude: </div>;
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
