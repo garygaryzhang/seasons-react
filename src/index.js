ba33e3c8b162b1597e6962c5cbc73600334a828f
JerseyKidG <cz284@njit.edu>
2020-01-15 21:56:13 -0600

    two types of component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+//functional based component
+/* const App = () => {
     window.navigator.geolocation.getCurrentPosition(
         position => console.log(position),
         err => console.log(err)
     );
 
 
-    return <div>Hi there!</div>;
-};
+    return <div>Latitude: </div>;
+}; */
 
+//class based component
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(
     <App />, 
