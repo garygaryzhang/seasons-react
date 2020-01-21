14fbfcc3479fa2708e7da73a7017c364be4ba48d
JerseyKidG <cz284@njit.edu>
2020-01-20 22:27:49 -0600

    Refactor data loading + Alternate State Initial

--- src/index.js
+++ src/index.js
@@ -2,28 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props); //a reference to the parents constructor function, must have this
-
-        //THIS IS THE ONLY TIME we do direct assignment to this.state
-        this.state = { lat: null, errorMessage: '' };
-
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // we called setstate !!!
-                this.setState({ lat: position.coords.latitude });
-
-                //we did not, never do this 
-                //this.state.lat = position.coords.latitude
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
-        console.log('My component was rendered to the screen');
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
     }
 
     componentDidUpdate() {
